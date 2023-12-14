a625fc143b446bd9c487c53153b4f641fd60b866
Marisol Park <marisolpark@10-18-174-175.dynapool.wireless.nyu.edu>
2023-12-14 08:39:04 -0500

    eleventh push

--- src/app/components/posts/index.js
+++ src/app/components/posts/index.js
@@ -5,7 +5,7 @@
     return (
         <div>
             <div className={styles.postContainer}>
-                <h4 className={styles.userNameStyle}>{post.username}'s diary entry:</h4>
+                <h4 className={styles.userNameStyle}>{post.username}s diary entry:</h4>
                 <p className={styles.postContStyle}>{post.postDate}</p>
                 <img src={post.imageURL} className={styles.imagepost} alt="" />
                 <p className={styles.postContStyle}>{post.postContent}</p>
